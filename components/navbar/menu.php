7bd14bd6404d069b9f3447e4a6aef1ff39c5fbb5
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-15 16:25:34 -0600

    Add navbar component with menu

---
+++ components/navbar/menu.php
@@ -0,0 +1,9 @@
+<ul class="navbar-nav">
+    <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+    </li>
+    <!-- Agrega más elementos según tus rutas y necesidades -->
+    <li class="nav-item">
+        <a class="nav-link" href="#">Gestión de Empleados</a>
+    </li>
+</ul>
