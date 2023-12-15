c803b459dcdd0497451d7fdeb8356d6c82b78e49
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-15 16:25:38 -0600

    Add footer and header components

---
+++ components/footer/default.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>&copy; <?= date("Y"); ?> Sistema de RRHH. Todos los derechos reservados.</p>
+    <!-- Aquí puedes agregar enlaces o información adicional -->
+</footer>
+</body>
+</html>
